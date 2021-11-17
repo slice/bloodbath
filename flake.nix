191cfd203a91d37ea1d333d551dbb7de43c93da2
slice <tinyslices@gmail.com>
2021-11-17 13:43:11 -0800

    remove naersk from flake

--- flake.nix
+++ flake.nix
@@ -1,74 +1,75 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    naersk = {
-      url = "github:nmattia/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, fenix, naersk, utils, nixpkgs }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages.bloodbath = (naersk.lib.${system}.override {
-          inherit (fenix.packages.${system}.stable) cargo rustc;
-        }).buildPackage {
-          name = "bloodbath";
-          src = ./.;
-          nativeBuildInputs = with pkgs;
-            [ openssl pkgconfig ] ++ nixpkgs.lib.optional pkgs.stdenv.isDarwin [
-              # needed by curl-sys
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
-        };
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    let
+      packages = flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in rec {
+          packages.bloodbath = (pkgs.makeRustPlatform {
+            inherit (fenix.packages.${system}.stable) cargo rustc;
+          }).buildRustPackage {
+            pname = "bloodbath";
+            version = (nixpkgs.lib.importTOML ./Cargo.toml).package.version;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ]
+              ++ nixpkgs.lib.optional pkgs.stdenv.isDarwin [
+                # needed by curl-sys
+                pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+              ];
+          };
 
-        defaultPackage = packages.bloodbath;
-      }) // {
-        nixosModule = { config, lib, pkgs, ... }:
-          with lib;
-          let cfg = config.services.bloodbath;
-          in {
-            options.services.bloodbath = {
-              enable = mkEnableOption "bloodbath";
+          defaultPackage = packages.bloodbath;
+        });
+    in packages // {
+      nixosModule = { config, lib, pkgs, ... }:
+        with lib;
+        let cfg = config.services.bloodbath;
+        in {
+          options.services.bloodbath = {
+            enable = mkEnableOption "bloodbath";
 
-              config = mkOption {
-                type = types.str;
-                default = "";
-                example = "";
-                description = "The configuration (written in TOML).";
-              };
-
-              timer = mkOption {
-                type = types.str;
-                default = "*:0/3";
-                example = "hourly";
-                description =
-                  "How often to run bloodbath (uses systemd calendar event syntax).";
-              };
+            config = mkOption {
+              type = types.str;
+              default = "";
+              example = "";
+              description = "The configuration (written in TOML).";
             };
 
-            config.systemd = mkIf cfg.enable {
-              timers.bloodbath = {
-                wantedBy = [ "timers.target" ];
-                partOf = [ "bloodbath.service" ];
-                timerConfig.OnCalendar = cfg.timer;
-              };
-
-              services.bloodbath = {
-                serviceConfig.Type = "oneshot";
-                after = [ "network-online.target" ];
-                wantedBy = [ "network-online.target" ];
-                script = "${self.defaultPackage.${pkgs.system}}/bin/bloodbath ${
-                    pkgs.writeText "config.toml" cfg.config
-                  }";
-              };
+            timer = mkOption {
+              type = types.str;
+              default = "*:0/3";
+              example = "hourly";
+              description =
+                "How often to run bloodbath (uses systemd calendar event syntax).";
             };
           };
-      };
+
+          config.systemd = mkIf cfg.enable {
+            timers.bloodbath = {
+              wantedBy = [ "timers.target" ];
+              partOf = [ "bloodbath.service" ];
+              timerConfig.OnCalendar = cfg.timer;
+            };
+
+            services.bloodbath = {
+              serviceConfig.Type = "oneshot";
+              after = [ "network-online.target" ];
+              wantedBy = [ "network-online.target" ];
+              script = "${self.defaultPackage.${pkgs.system}}/bin/bloodbath ${
+                  pkgs.writeText "config.toml" cfg.config
+                }";
+            };
+          };
+        };
+    };
 }
